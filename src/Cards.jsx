64e5fc81d1cfc0da3d2da44c7a981f43cced4a06
VISHESH JAIN <visheshjain@VISHESHs-MacBook-Air.local>
2021-05-21 15:51:42 +0530

    completed

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -7,37 +7,37 @@ function Cards(){
       <div className="row">
 
           <div className="col-lg-3 col-md-6 col-sm-12" >
-              <Card src={cardInfo[0].src} title={cardInfo[0].title} desc={cardInfo[0].desc}/>
+              <Card id="0" src={cardInfo[0].src} title={cardInfo[0].title} desc={cardInfo[0].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[1].src} title={cardInfo[1].title}  desc={cardInfo[1].desc}/>
+              <Card id="1" src={cardInfo[1].src} title={cardInfo[1].title}  desc={cardInfo[1].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[2].src} title={cardInfo[2].title}  desc={cardInfo[2].desc}/>
+              <Card id="2" src={cardInfo[2].src} title={cardInfo[2].title}  desc={cardInfo[2].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[3].src} title={cardInfo[3].title} desc={cardInfo[3].desc}/>
+              <Card id="3" src={cardInfo[3].src} title={cardInfo[3].title} desc={cardInfo[3].desc}/>
           </div>
 
-    </div> 
+    </div>
 
 
       <div className="row">
 
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[4].src} title={cardInfo[4].title} desc={cardInfo[4].desc}/>
+              <Card id="4" src={cardInfo[4].src} title={cardInfo[4].title} desc={cardInfo[4].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[5].src} title={cardInfo[5].title} desc={cardInfo[5].desc}/>
+              <Card id="5" src={cardInfo[5].src} title={cardInfo[5].title} desc={cardInfo[5].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[6].src} title={cardInfo[6].title} desc={cardInfo[6].desc}/>
+              <Card id="6" src={cardInfo[6].src} title={cardInfo[6].title} desc={cardInfo[6].desc}/>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-              <Card src={cardInfo[7].src} title={cardInfo[7].title} desc={cardInfo[7].desc}/>
+              <Card id="7" src={cardInfo[7].src} title={cardInfo[7].title} desc={cardInfo[7].desc}/>
           </div>
 
-       </div>    
+       </div>
     </div>)
 }
 
--- src/cardInfo.js
+++ src/cardInfo.js
@@ -1,22 +1,22 @@
 var cardInfo= [
-    
-    {title:'JAVA MASTER', desc:'Learn Data structures and Algorithms right from the basics and become an accomplished programmer using Java',src: ' https://cdn.iconscout.com/icon/free/png-512/java-43-569305.png'},
-    {title:'C++ BASICS',  desc:'Learn Data structures and Algorithms right from the basics and become an accomplished programmer using C++',src:'https://download.logo.wine/logo/C%2B%2B/C%2B%2B-Logo.wine.png'},
+
+    {title:'JAVA MASTER', video:'https://www.youtube.com/embed/mAtkPQO1FcA',desc:'Learn Data structures and Algorithms right from the basics and become an accomplished programmer using Java',src: ' https://cdn.iconscout.com/icon/free/png-512/java-43-569305.png'},
+    {title:'C++ BASICS', video:'https://www.youtube.com/embed/kJkB_Tggk8U', desc:'Learn Data structures and Algorithms right from the basics and become an accomplished programmer using C++',src:'https://download.logo.wine/logo/C%2B%2B/C%2B%2B-Logo.wine.png'},
 
 
 
-{title:'REACT JS', desc:'ReactJS offers graceful solutions to some of front-end programming’s most persistent issues, allowing you to build dynamic apps', src:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'},
+{title:'REACT JS', video:'https://www.youtube.com/embed/N3AkSS5hXMA',desc:'ReactJS offers graceful solutions to some of front-end programming’s most persistent issues, allowing you to build dynamic apps', src:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'},
 
-{title:'NODE JS', desc:'This course help students aspiring to become Web devs learn FrontEnd and BackEnd Web development with the help of nodejs', src:'https://ih1.redbubble.net/image.1637718590.1604/st,small,845x845-pad,1000x1000,f8f8f8.jpg'},
+{title:'NODE JS',video:'https://www.youtube.com/embed/2gQG4cFjahw', desc:'This course help students aspiring to become Web devs learn FrontEnd and BackEnd Web development with the help of nodejs', src:'https://ih1.redbubble.net/image.1637718590.1604/st,small,845x845-pad,1000x1000,f8f8f8.jpg'},
 
-{title:'PYTHON ADVANCE', desc:'This course will revolve around the concepts of Python, ranging from basic to advanced concepts and development of applications .', src:'https://images.ctfassets.net/mrop88jh71hl/55rrbZfwMaURHZKAUc5oOW/9e5fe805eb03135b82e962e92169ce6d/python-programming-language.png'},
+{title:'PYTHON ADVANCE', video:'https://www.youtube.com/embed/UGj0bOYPdCw',desc:'This course will revolve around the concepts of Python, ranging from basic to advanced concepts and development of applications .', src:'https://images.ctfassets.net/mrop88jh71hl/55rrbZfwMaURHZKAUc5oOW/9e5fe805eb03135b82e962e92169ce6d/python-programming-language.png'},
 
-{ title:'MACHINE LEARNING', desc:'This course will provide a great kick-start by starting with Python, Data Visualization, Scraping, Data Cleaning and applying various ML Algos.',src:'https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg'},
+{ title:'MACHINE LEARNING',video:'https://www.youtube.com/embed/ukzFI9rgwfU', desc:'This course will provide a great kick-start by starting with Python, Data Visualization, Scraping, Data Cleaning and applying various ML Algos.',src:'https://florencefennel.co.in/image/cache/catalog/florencefennel/Machine--Learning-1000x1000.jpg'},
 
-{ title:'DATA SCIENCE', desc:'This course will provide a great kick-start by starting with Python, Data Visualization, Scraping, Data Cleaning and applying various ML Algos ',src:'https://i.pinimg.com/736x/b6/4f/4b/b64f4b63ad2389110f5d523fe2ae997c.jpg'},
+{ title:'DATA SCIENCE',video:'https://www.youtube.com/embed/X3paOmcrTjQ', desc:'This course will provide a great kick-start by starting with Python, Data Visualization, Scraping, Data Cleaning and applying various ML Algos ',src:'https://i.pinimg.com/736x/b6/4f/4b/b64f4b63ad2389110f5d523fe2ae997c.jpg'},
 
-{ title:'ANDROID', desc:'Android powers over 80 percent of the world’s smartphones, and represents an incredible opportunity for developers everywhere',src:'https://1000logos.net/wp-content/uploads/2016/10/Android-Logo.png'},
+{ title:'ANDROID',video:'https://www.youtube.com/embed/_TgrWBZ40cs', desc:'Android powers over 80 percent of the world’s smartphones, and represents an incredible opportunity for developers everywhere',src:'https://1000logos.net/wp-content/uploads/2016/10/Android-Logo.png'},
 
-]    
+]
 
 export default cardInfo;
